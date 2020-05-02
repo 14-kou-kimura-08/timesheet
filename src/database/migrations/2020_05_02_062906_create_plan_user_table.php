53f1e500139f2b06279cee674aac141214b84902
kimura_kouichirou <814.kimura86@gmail.com>
2020-05-02 15:48:36 +0900

    create migration

---
+++ src/database/migrations/2020_05_02_062906_create_plan_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plan_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->constrained('company_plans');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plan_user');
+    }
+}
---
+++ src/database/migrations/2020_05_02_063030_create_timesheets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimesheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->constrained('company_plans');
+            $table->foreignId('user_id')->constrained();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timesheets');
+    }
+}
