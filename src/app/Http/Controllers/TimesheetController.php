4a36433103f0d4e2db1b8688c82974d0b4a0a94f
kimura_kouichirou <814.kimura86@gmail.com>
2020-05-03 09:27:19 +0900

    Create timesheet consider holiday requests.

--- src/app/Http/Controllers/TimesheetController.php
+++ src/app/Http/Controllers/TimesheetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Company;
+use App\HolidayRequests;
 use App\Plan;
 use App\Timesheet;
 use Carbon\Carbon;
@@ -27,17 +28,24 @@ class TimesheetController extends Controller
     protected $plan;
 
     /**
+     * The holiday requests repository instance.
+     */
+    protected $holidayRequests;
+
+    /**
      * Create a new controller instance.
      *
      * @param Timesheet $timesheets
      * @param Company $company
      * @param Plan $plan
+     * @param HolidayRequests $holidayRequests
      */
-    public function __construct(Timesheet $timesheets, Company $company, Plan $plan)
+    public function __construct(Timesheet $timesheets, Company $company, Plan $plan, HolidayRequests $holidayRequests)
     {
-        $this->timesheets = $timesheets;
-        $this->company    = $company;
-        $this->plan       = $plan;
+        $this->timesheets      = $timesheets;
+        $this->company         = $company;
+        $this->plan            = $plan;
+        $this->holidayRequests = $holidayRequests;
     }
 
     public function index()
@@ -58,9 +66,21 @@ class TimesheetController extends Controller
         // 指定された年月の最終日を取得
         $lastOfMonth = Carbon::create($validatedData['year'], $validatedData['month'])->lastOfMonth();
 
+        // 休み申請を取得
+        $holidayRequests = $this->holidayRequests->where('company_id', 1)
+            ->where('date', '>=' , $firstOfMonth->format('Y-m-d'))
+            ->where('date', '<=' , $lastOfMonth->format('Y-m-d'))
+            ->get();
+
+        // 休み申請を整形
+        $transformedHolidayRequests = [];
+        foreach ($holidayRequests as $holidayRequest) {
+            $transformedHolidayRequests[$holidayRequest->date][] = $holidayRequest->user_id;
+        }
+
         // 指定された年月の初日と最終日をつかってループ処理をする
         // エラーが発生したときに途中で作成されたデータを破棄するためにトランザクションを利用
-        DB::transaction(function () use ($firstOfMonth, $lastOfMonth) {
+        DB::transaction(function () use ($firstOfMonth, $lastOfMonth, $transformedHolidayRequests) {
             for ($date = $firstOfMonth; $date <= $lastOfMonth; $date->addDay()) {
                 // $dayは何曜日か
                 $dayName = $date->isoFormat('dddd');
@@ -71,17 +91,25 @@ class TimesheetController extends Controller
 
                 // そのplanをOKと言っているひとは誰か
                 foreach ($plans as $plan) {
-                    // 会社が必要な人数よりも多いかどうかで処理を変える、そうしないとエラーになる
-                    if ($plan->users->count() >= $plan->number_of_people) {
-                        $users = $plan->users->random($plan->number_of_people);
+                    // 休み申請がある日かどうかチェック、休み申請があればシフトに入れるユーザーから除外
+                    if (array_key_exists($date->format('Y-m-d'), $transformedHolidayRequests)) {
+                        $usersWhoCanWork = $plan->users->except($transformedHolidayRequests[$date->format('Y-m-d')]);
                     } else {
-                        $users = $plan->users->random($plan->users->count());
+                        $usersWhoCanWork = $plan->users;
                     }
+
+                    // 会社が必要な人数よりも多いかどうかで処理を変える、そうしないとエラーになる
+                    if ($usersWhoCanWork->count() >= $plan->number_of_people) {
+                        $workers = $usersWhoCanWork->random($plan->number_of_people);
+                    } else {
+                        $workers = $usersWhoCanWork->random($usersWhoCanWork->count());
+                    }
+
                     // データ追加する、もし対象ユーザーがいなくてもループが回らないのでエラーにはならない
-                    foreach ($users as $user) {
+                    foreach ($workers as $worker) {
                         $this->timesheets->create([
                             'plan_id' => $plan->id,
-                            'user_id' => $user->id,
+                            'user_id' => $worker->id,
                             'date'    => $date->format('Y-m-d'),
                         ]);
                     }
